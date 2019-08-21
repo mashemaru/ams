78a2473ee80d7cf5c40e9b392e51405ee0f2451b
Mash <kawaiimash@gmail.com>
2019-08-21 22:15:37 +0800

    Removed preset bootstrap

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$4zv8LEcuZg5F6vKFxJws7.0LB6pZ9larDbVFlAq/RU9qSeYfKS6cS', // dev123
         'remember_token' => Str::random(10),
     ];
 });
