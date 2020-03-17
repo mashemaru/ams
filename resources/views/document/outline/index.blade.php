aba334a09576a3eb3b8b28cdc8d1eb4044a3030b
Derald <deralddlared@gmail.com>
2020-03-17 16:13:21 +0800

    Add children view with accordion interaction for document outline.

--- resources/views/document/outline/index.blade.php
+++ resources/views/document/outline/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app', ['title' => __('Document Outlines Management')])
+@php
 
+function buildTree($elements, $parentId = 0) {
+    // print_r($elements);
+    $branch = array();
+// die();
+    foreach ($elements as $element) {
+        if ($element['parent_id'] == $parentId) {
+            $children = buildTree($elements, $element['id']);
+            if ($children) {
+                $element['children'] = $children;
+            }
+            $branch[] = $element;
+        }
+    }
+
+    return $branch;
+}
+@endphp
 @section('content')
     @include('layouts.headers.cards')
 
@@ -21,7 +39,7 @@
                         </div>
                     </div>
 
-                    <div class="table-responsive">
+                    <div class="table-responsive doc_outline">
                         <table class="table align-items-center table-flush">
                             <thead class="thead-light">
                                 <tr>
@@ -32,10 +50,94 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($accreditation_doc->outlines as $doc)
-                                    <tr>
-                                        <td>{{ $accreditation_doc->document->document_name }}</td>
-                                        <td>{{ $doc->section }}</td>
+                                @php
+                                    $outlines = buildTree($accreditation_doc->outlines);
+                                @endphp
+                                @foreach ($outlines as $outline)
+                              
+                                @if($outline->children)
+                                <tr class="clickable-row parent_outline" data-toggle="collapse" id="row{{$outline->id}}" data-target=".row{{$outline->id}}" style="cursor: pointer;">
+                                        <td>
+                                        {{ $accreditation_doc->document->document_name }} -- {{$outline->id}}
+                                        <i class="glyphicon glyphicon-plus"></i></td>
+                                        <td>{{ $outline->section }}</td>
+                                        <td>{{ $outline->doc_type }}</td>
+                                        <td class="text-right">
+                                            <div class="dropdown">
+                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-v"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                    <a class="dropdown-item" href="{{ route('document-outline.edit', $outline) }}">{{ __('Edit') }}</a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    
+                                    </tr>
+                                    
+                                    @foreach($outline->children as $child)
+                                    <tr class="clickable collapse row{{$outline->id}}" data-toggle="collapse" id="row{{$outline->id}}" data-target=".row{{$outline->id}}">
+                                        <td>
+                                        {{ $accreditation_doc->document->document_name }}
+                                        <i class="glyphicon glyphicon-plus"></i></td>
+                                        <td>{{$child->section}}</td>
+                                        <td>{{ $outline->doc_type }}</td>
+                                        <td class="text-right">
+                                            <div class="dropdown">
+                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-v"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                    <a class="dropdown-item" href="{{ route('document-outline.edit', $outline) }}">{{ __('Edit') }}</a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    
+                                    </tr>
+                                    @endforeach
+                                @endif
+                                    <!-- <tr class="clickable-row" data-toggle="collapse" id="row{{$outline->id}}" data-target=".row{{$outline->id}}">
+                                        <td>
+                                        <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample{{$outline->id}}" role="button" aria-expanded="false" aria-controls="collapseExample{{$outline->id}}">
+                                            Link with href
+                                        </a>
+                                        {{ $accreditation_doc->document->document_name }} --- {{$outline->id}}</td>
+                                        <td>{{ $outline->section }} --- {{$outline->root_parent_id}}  </td>
+                                        <td>{{ $outline->doc_type }}</td>
+                                        <td class="text-right">
+                                            <div class="dropdown">
+                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-v"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                    <a class="dropdown-item" href="{{ route('document-outline.edit', $outline) }}">{{ __('Edit') }}</a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @if($outline->children)
+                                    
+                                        <tr class="clickable collapse row{{$outline->id}}" data-toggle="collapse" id="row{{$outline->id}}" data-target=".row{{$outline->id}}">
+                                        <td><i class="glyphicon glyphicon-plus"></i></td>
+                                        <td>Supplier2</td>
+                                        <td>Supplier2</td>
+                                        <td>Supplier2</td>
+                                    
+                                    </tr>
+                                    @endif -->
+                                
+                                @endforeach
+
+                                <!-- @foreach ($accreditation_doc->outlines as $doc)
+                                
+                                @if($doc->root_parent_id == $doc->parent_id)
+                                    <tr class="clickable-row" data-toggle="collapse" id="row{{$doc->id}}" data-target=".row{{$doc->id}}">
+                                        <td>
+                                        <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample{{$doc->id}}" role="button" aria-expanded="false" aria-controls="collapseExample{{$doc->id}}">
+                                            Link with href {{$doc->id}}
+                                        </a>
+                                        {{ $accreditation_doc->document->document_name }} --- {{$doc->id}}</td>
+                                        <td>{{ $doc->section }} --- {{$doc->root_parent_id}}  @if($doc->root_parent_id == 328) --- derald pogi @endif</td>
                                         <td>{{ $doc->doc_type }}</td>
                                         <td class="text-right">
                                             <div class="dropdown">
@@ -48,7 +150,17 @@
                                             </div>
                                         </td>
                                     </tr>
-                                @endforeach
+                                    
+                                        <tr class="clickable collapse row{{$doc->id}}" data-toggle="collapse" id="row{{$doc->id}}" data-target=".row{{$doc->id}}">
+                                        <td><i class="glyphicon glyphicon-plus"></i></td>
+                                        <td>Supplier2</td>
+                                        <td>Supplier2</td>
+                                        <td>Supplier2</td>
+                                    
+                                    </tr>
+                                    
+                                @endif
+                                @endforeach -->
                             </tbody>
                         </table>
                     </div>
